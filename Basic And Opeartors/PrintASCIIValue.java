26a3cbf374bcb882dc69bb25f296733218f78f8e
Basant Kumar Mahato <basantkumar72580@gmail.com>
2025-09-04 16:44:36 +0530

    Initial Commit

--- Basic And Opeartors/PrintASCIIValue.java
+++
@@ -1,7 +0,0 @@
-public class PrintASCIIValue {
-    public static void main(String[] args) {
-        char ch = 'A'; // Example character
-        int asciiValue = (int) ch; // Casting char to int to get ASCII value
-        System.out.println("The ASCII value of '" + ch + "' is: " + asciiValue);
-    }
-}
